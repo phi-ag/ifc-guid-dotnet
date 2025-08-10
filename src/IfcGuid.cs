d3a9be1626c7743eb5e338a03086c0e8fd791d68
Peter Hirn <peter.hirn@phi.ag>
2025-08-10 10:27:09 +0200

    chore: init
    Release-As: 0.0.1

---
+++ src/IfcGuid.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections.Immutable;
+using System.Runtime.CompilerServices;
+using System.Text.RegularExpressions;
+
+public static class IfcGuid
+{
+    private const string IfcGuidPattern = @"^[0-3][\dA-Za-z_$]{21}$";
+
+#if NET
+    private static readonly Regex IfcGuidRegex =
+        new(IfcGuidPattern, RegexOptions.Compiled | RegexOptions.NonBacktracking);
+#else
+    private static readonly Regex IfcGuidRegex =
+        new(IfcGuidPattern, RegexOptions.Compiled, TimeSpan.FromMilliseconds(100));
+#endif
+
+    private const string Chars =
+        "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz_$";
+
+    private static readonly ImmutableDictionary<char, int> Reverse =
+        Chars.Select((c, index) => new KeyValuePair<char, int>(c, index)).ToImmutableDictionary();
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private static void FixGuidByteOrder(Span<byte> guid)
+    {
+        if (BitConverter.IsLittleEndian)
+        {
+            guid.Slice(0, 4).Reverse();
+            guid.Slice(4, 2).Reverse();
+            guid.Slice(6, 2).Reverse();
+        }
+    }
+
+    public static string ToIfcGuid(Guid input)
+    {
+#if NET
+        Span<byte> guid = stackalloc byte[16];
+        if (!input.TryWriteBytes(guid))
+            throw new InvalidOperationException("Failed to convert GUID to bytes");
+#else
+        Span<byte> guid = input.ToByteArray();
+#endif
+
+        FixGuidByteOrder(guid);
+
+        Span<char> result = stackalloc char[22];
+        result[0] = Chars[(guid[0] >> 6) & 63];
+        result[1] = Chars[guid[0] & 63];
+
+        for (int i = 1, j = 2; i < 16; i += 3, j += 4)
+        {
+            var u24 = (guid[i] << 16) | (guid[i + 1] << 8) | guid[i + 2];
+            result[j] = Chars[(u24 >> 18) & 63];
+            result[j + 1] = Chars[(u24 >> 12) & 63];
+            result[j + 2] = Chars[(u24 >> 6) & 63];
+            result[j + 3] = Chars[u24 & 63];
+        }
+
+        return result.ToString();
+    }
+
+    public static Guid FromIfcGuid(string ifcGuid)
+    {
+        if (ifcGuid is null) throw new ArgumentNullException(nameof(ifcGuid));
+        if (ifcGuid.Length != 22) throw new ArgumentException("Invalid IFC-GUID length");
+        if (!IfcGuidRegex.IsMatch(ifcGuid)) throw new ArgumentException("Invalid character in IFC-GUID");
+
+        Span<byte> result = stackalloc byte[16];
+        result[0] = (byte)((Reverse[ifcGuid[0]] << 6) | Reverse[ifcGuid[1]]);
+
+        for (int i = 2, j = 1; j < 16; i += 4, j += 3)
+        {
+            var u24 =
+              (Reverse[ifcGuid[i]] << 18) |
+              (Reverse[ifcGuid[i + 1]] << 12) |
+              (Reverse[ifcGuid[i + 2]] << 6) |
+              Reverse[ifcGuid[i + 3]];
+
+            result[j] = (byte)((u24 >> 16) & 255);
+            result[j + 1] = (byte)((u24 >> 8) & 255);
+            result[j + 2] = (byte)(u24 & 255);
+        }
+
+        FixGuidByteOrder(result);
+
+#if NET
+        return new(result);
+#else
+        return new(result.ToArray());
+#endif
+    }
+}
---
+++ tests/IfcGuid.cs
@@ -0,0 +1,114 @@
+﻿using NUnit.Framework;
+using static IfcGuid;
+
+#if NET
+using FsCheck;
+using PropertyAttribute = FsCheck.NUnit.PropertyAttribute;
+#endif
+
+#if NET
+namespace Tests.Core;
+#else
+namespace Tests.Framework;
+#endif
+
+[TestFixture]
+public class IfcGuidTests
+{
+    [Test]
+    public void Example_Guid_v4()
+    {
+        var ifc = "0VGQug_k98B9cf4GSEmT_F";
+        var guid = new Guid("1f41ae2a-fae2-482c-99a9-11070ec1df8f");
+
+        using (Assert.EnterMultipleScope())
+        {
+            Assert.That(ToIfcGuid(guid), Is.EqualTo(ifc));
+            Assert.That(FromIfcGuid(ifc), Is.EqualTo(guid));
+        }
+    }
+
+    [Test]
+    public void Example_Guid_v7()
+    {
+        var ifc = "01bhO9fsz_RxNh9a_y9jls";
+        var guid = new Guid("0196b609-a76f-7e6f-b5eb-264fbc26dbf6");
+
+        using (Assert.EnterMultipleScope())
+        {
+            Assert.That(ToIfcGuid(guid), Is.EqualTo(ifc));
+            Assert.That(FromIfcGuid(ifc), Is.EqualTo(guid));
+        }
+    }
+
+    [Test]
+    public void Limits()
+    {
+        var ifcNil = "0000000000000000000000";
+        var guidNil = new Guid("00000000-0000-0000-0000-000000000000");
+
+        var ifcMax = "3$$$$$$$$$$$$$$$$$$$$$";
+        var guidMax = new Guid("ffffffff-ffff-ffff-ffff-ffffffffffff");
+
+        using (Assert.EnterMultipleScope())
+        {
+            Assert.That(ToIfcGuid(guidNil), Is.EqualTo(ifcNil));
+            Assert.That(FromIfcGuid(ifcNil), Is.EqualTo(guidNil));
+
+            Assert.That(ToIfcGuid(guidMax), Is.EqualTo(ifcMax));
+            Assert.That(FromIfcGuid(ifcMax), Is.EqualTo(guidMax));
+        }
+    }
+
+    [Test]
+    public void Null()
+    {
+        Assert.Throws(Is.TypeOf<ArgumentNullException>()
+            .And.Message.StartWith("Value cannot be null."),
+            () => FromIfcGuid(null!));
+    }
+
+    [Test]
+    public void Invalid_Length()
+    {
+        var tooLong = "01bhO9fsz_RxNh9a_y9jls_";
+        var tooShort = "01bhO9fsz_RxNh9a_y9jl";
+
+        using (Assert.EnterMultipleScope())
+        {
+            Assert.Throws(Is.TypeOf<ArgumentException>()
+                .And.Message.EqualTo("Invalid IFC-GUID length"),
+                () => FromIfcGuid(tooLong));
+
+            Assert.Throws(Is.TypeOf<ArgumentException>()
+                .And.Message.EqualTo("Invalid IFC-GUID length"),
+                () => FromIfcGuid(tooShort));
+        }
+    }
+
+    [Test]
+    public void Invalid_Character()
+    {
+        var containsEquals = "01bhO9fsz_RxNh9a_y9jl=";
+        var invalidFirstCharacter = "41bhO9fsz_RxNh9a_y9jls";
+
+        using (Assert.EnterMultipleScope())
+        {
+            Assert.Throws(Is.TypeOf<ArgumentException>()
+                .And.Message.EqualTo("Invalid character in IFC-GUID"),
+                () => FromIfcGuid(containsEquals));
+
+            Assert.Throws(Is.TypeOf<ArgumentException>()
+                .And.Message.EqualTo("Invalid character in IFC-GUID"),
+                () => FromIfcGuid(invalidFirstCharacter));
+        }
+    }
+
+#if NET
+    [Property(MaxTest = 1_000)]
+    public void Check(Guid guid)
+    {
+        Assert.That(FromIfcGuid(ToIfcGuid(guid)), Is.EqualTo(guid));
+    }
+#endif
+}
